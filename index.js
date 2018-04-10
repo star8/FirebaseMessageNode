abec71678a2c765a1a009ef678f554c36ee0564d
INTELP1-PC\samsungwin <souravrec@gmail.com>
2018-04-11 02:12:48 +0530

    removed node gcm

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var gcm = require('node-gcm');
+
 var request = require('request');
 
 var  PRIORITY = {
@@ -72,17 +72,8 @@ var COLLAPSE_KEY = "collapse_key";
 		 */
 
 		var PARAM_HEADER_SERVER_KEY = "Authorization";
-		/**
-		 * Prefix of the topic.
-		 */
-		var PARAM_REGISTRATION_IDS = "registration_ids";
-		/**
-		 * Missing registration_id. Sender should always add the registration_id
-		 * to the request.
-		 */
-		var ERROR_MISSING_REGISTRATION = "MissingRegistration";
-
-
+  
+    
 var sender
 
 var registrationTokens = [];
@@ -98,59 +89,19 @@ SENDER_ID = json.SENDER_ID
 var data = {}
 
 
-  message = new gcm.Message({
-    collapseKey: !json.collapseKey?COLLAPSE_KEY:json.collapseKey ,
-    priority: !json.priority?PRIORITY.NORMAL:json.priority,
-    contentAvailable: !json.contentAvailable?true:json.contentAvailable,
-    delayWhileIdle: !json.delayWhileIdle?false:json.delayWhileIdle,
-    timeToLive: !json.timeToLive?DEFAUTL_TIME_TO_LIVE:json.timeToLive,   
-    dryRun: !json.dryRun?true:json.dryRun,
-   // data: data,
-   // notification:notification
-});
-
- message = new gcm.Message({
-    collapseKey: !json.collapseKey?COLLAPSE_KEY:json.collapseKey ,
-    priority: !json.priority?PRIORITY.NORMAL:json.priority,
-    contentAvailable: !json.contentAvailable?true:json.contentAvailable,
-    delayWhileIdle: !json.delayWhileIdle?false:json.delayWhileIdle,
-    timeToLive: !json.timeToLive?DEFAUTL_TIME_TO_LIVE:json.timeToLive,   
-    dryRun: !json.dryRun?true:json.dryRun,
-   // data: data,
-   // notification:notification
-});
+  
 
 
 
 
-if(json.data){
-
-  let keyArr  = Object.keys(json.data),
-  dataArr = [];
-
-  for (var i in keyArr) {
-    let keyName = keyArr[i]  
-    var keyValue = json.data[keyArr[i]]
-    var jsonRec = {}
-    jsonRec[keyName] = keyValue  
-    console.log("keyName =="+keyName )
-    console.log("keyValue =="+keyValue )
-    message.addData(keyName,keyValue);
-    }
-}
 
 
-console.log("message data  == "+JSON.stringify(message.data))
 
 if(json.notification){
-  //notification.title=json.notification.title
-  //notification.icon=json.notification.icon
-  //notification.body=json.notification.body
+  notification.title=json.notification.title
+  notification.icon=json.notification.icon
+  notification.body=json.notification.body
 
-  // Add notification payload as key value
-message.addNotification('title', json.notification.title);
-message.addNotification('body', json.notification.body);
-message.addNotification('icon', json.notification.icon);
 }
 
 
@@ -159,6 +110,38 @@ if(json.registrationIDs){
     registrationTokens.push(json.registrationIDs[j])
   }
 }
+
+
+message = {  
+  priority: !json.priority?PRIORITY.NORMAL:json.priority,
+  contentAvailable: !json.contentAvailable?true:json.contentAvailable,
+  delay_while_idle: !json.delayWhileIdle?false:json.delayWhileIdle,
+  time_to_live: !json.timeToLive?DEFAUTL_TIME_TO_LIVE:json.timeToLive,     
+}
+if(json.contentAvailable){
+  message.content_available = json.contentAvailable
+}
+if(registrationTokens && registrationTokens.length==1){
+  message.to = registrationTokens[0]
+}
+
+if(registrationTokens && registrationTokens.length>1){
+  message.registration_ids = registrationTokens
+}
+
+if(json.data){ 
+  message.data = json.data 
+}
+
+if(json.notification){ 
+  message.notification = json.notification     
+}
+
+if(json.collapsible){ 
+  message.collapse_key = collapse_key
+}
+
+
 
 messageCreateDeviceGroup = {
     
@@ -189,45 +172,6 @@ messageAddDeviceGroup = {
 
 };
 
-requestOptionsDeviceGroup ={
-  timeout : DEFAUTL_CONNECTION_TIMEOUT,
-  url: FCM_DEVICE_GROUP_ENDPOINT,
-headers:[
-  {
-    name: 'accept',
-    value: HEADER_CONTENT_TYPE_JSON
-  },
-  {
-    name: 'content-type',
-    value: HEADER_CONTENT_TYPE_JSON
-  },
-  {
-    name: PARAM_HEADER_SERVER_KEY,
-    value: "key=" + SERVER_KEY
-  },
-  {
-    name: 'project_id',
-    value: SENDER_ID
-  }
-]
-}
-
-
-
-requestOptions ={
-  timeout : DEFAUTL_CONNECTION_TIMEOUT,
-  url: FCM_SEND_ENDPOINT,
-headers:[
-  {
-    name: 'content-type',
-    value: HEADER_CONTENT_TYPE_JSON
-  },
-  {
-    name: PARAM_HEADER_SERVER_KEY,
-    value: "key=" + SERVER_KEY
-  }
-]
-}
 
 console.log("registrationTokens  == "+JSON.stringify(registrationTokens))
 
@@ -235,12 +179,29 @@ console.log("registrationTokens  == "+JSON.stringify(registrationTokens))
 
 
   exports.send=function(){
-    sender = new gcm.Sender(SERVER_KEY,requestOptions);
+
     console.log("message  == "+JSON.stringify(message))
     
-    sender.send(message, { registrationTokens: registrationTokens }, function (err, response) {
-      if(err) console.error(err);
-      else    console.log(response);
+    request({
+      url: FCM_SEND_ENDPOINT,
+      timeout : DEFAUTL_CONNECTION_TIMEOUT,
+      method: 'POST',      
+      headers: {
+        'content-Type' :HEADER_CONTENT_TYPE_JSON,
+        'Authorization': "key=" + SERVER_KEY
+        
+      },      
+      body: JSON.stringify(message)}    
+      
+    , function(error, response, body) {
+      if (error) { 
+        console.error(error, response, body); 
+      }      
+      else {
+        //console.log('Done! response = ')
+        console.log(JSON.stringify(response))
+        //return response.notification_key
+      }
     });
      
   }
--- runindex.js
+++ runindex.js
@@ -1,17 +1,13 @@
 
 var firebaseMsg = require('./index.js')
 
-firebaseMsg.initilize({SERVER_KEY:'AIzaSyDZGXzG1u3nSvCXpcCXJLEnwGkYWpkG3uc',SENDER_ID:'582092791978',collapseKey: 'demo',
+firebaseMsg.initilize({SERVER_KEY:'AIzaSyDZGXzG1u3nSvCXpcCXJLEnwGkYWpkG3uc',SENDER_ID:'582092791978',collapsible: false,
 priority: 'high',contentAvailable: true,delayWhileIdle: true,timeToLive: 3,dryRun: false,
 
 notification: {
 	title: "Dummy Title",
 	icon: "test icon ",
 	body: "This is a notification that will be displayed if your app is in the background."
-},
-data: {
-	key1: 'message1',
-	key2: 'message2'
 },
 registrationIDs:['c-dHuLZ0yRI:APA91bFGllyyYCfgE_948duZGnYOPTiAm3WImxKpTxDFsLIMJlIzW4-A9JplMa3CwQCDX-tUDC2qBVh1L2SSH3GUjlGhdVBkF5j7LWQYSAKiWCVmwTqlY0zZJsmhFw90BbwaonfjGuw3']
 ,
@@ -23,6 +19,9 @@ notification_key_name:"1234_3"
 firebaseMsg.send();
 
 //var notification_key = firebaseMsg.createDeviceGroup();
+
+//console.log(notification_key)
+
 
 //firebaseMsg.removeDeviceGroup()
 
